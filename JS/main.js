b00e842458dd85d1f8a47098fc42c1588ac23eeb
Ivan Penev <ivan.penev92@gmail.com>
2019-02-20 19:11:09 +0100

    info and add

--- JS/main.js
+++ JS/main.js
@@ -61,3 +61,31 @@ const ricknillos = new TokenContract(rick);
 ricknillos.setName("Ricknillos");
 ricknillos.setSymbol("RNiLL");
 ricknillos.setTotalSupply(100);
+
+console.log("\n" + "Contrato de los Ricknillos" + "\n" + "==========================");
+ricknillos.info();
+
+/**
+* El contrato TokenContract contiene una tabla de balances 
+* de token por propietario:
+* mapping(propietario => numero de unidades que posee)
+* Permite al contrato llevar el seguimiento de quien
+* posee las entradas.
+* Cada transferencia de entradas entre propietarios es 
+* una deducción en un balance y una adicion en el otro.
+* 
+* Crea una tabla "balances" que mapee cada propietario 
+* (su Public Key) al numero de tokens que posee.
+* Añade a Rick con sus 100 entradas.
+* Asegurate de que si el propietario (su Public Key) 
+* ya existe en la tabla, sus unidades no se actualicen.
+* 
+* addOwner()
+* añade el propietario inicial de todos los tokens de este contrato
+* @param PublicKey del propietario
+* @param cantidad de tokens que posee
+*/
+
+ricknillos.addOwner(rick.pk, ricknillos.totalSupply);
+ricknillos.addOwner(morty.pk, 0);
+console.log(ricknillos.balances);
--- JS/token.js
+++ JS/token.js
@@ -11,5 +11,14 @@ class TokenContract {
 		this.setTotalSupply = function(totalSupply) {
 			this.totalSupply = totalSupply;
 		}
+		this.info = function() {
+			console.log("name = ", this.name);
+			console.log("symbol = ", this.symbol);
+			console.log("totalSupply = ", this.totalSupply);
+			console.log("owner PK = ", this.owner.pk);
+		}
+		this.addOwner = function(pk, tokens) {
+			this.balances[pk] = tokens;
+		}
 	}
 }
